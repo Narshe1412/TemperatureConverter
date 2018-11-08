b77e9b44aa4d5b9cb0d5ba216733b62452c07f60
Manuel Colorado <manuel1412@gmail.com>
2018-11-08 20:34:06 +0000

    Created listeners for radio buttons so they disable when the same on the other side is pressed

--- app/src/main/java/mcolorado/temperatureconverter/CalculatorActivity.java
+++ app/src/main/java/mcolorado/temperatureconverter/CalculatorActivity.java
@@ -1,13 +1,113 @@
 package mcolorado.temperatureconverter;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.Toast;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private Button convertBtn;
+    private RadioGroup toGroup;
+    private RadioGroup fromGroup;
+    private EditText inputText;
+    private double tempResult;
+    private TemperatureTransform calc;
+    private RadioButton fromFarenheit;
+    private RadioButton fromCelsius;
+    private RadioButton fromKelvin;
+    private RadioButton toFarenheit;
+    private RadioButton toCelsius;
+    private RadioButton toKelvin;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        inputText = findViewById(R.id.txtTempInput);
+        convertBtn = findViewById(R.id.convertBtn);
+        fromGroup = findViewById(R.id.grpOptionsFrom);
+        fromFarenheit = findViewById(R.id.radioFarenheitFrom);
+        fromCelsius = findViewById(R.id.radioCelsiusFrom);
+        fromKelvin = findViewById(R.id.radioKelvinFrom);
+        toGroup = findViewById(R.id.grpOptionsTo);
+        toFarenheit = findViewById(R.id.radioFarenheitTo);
+        toCelsius = findViewById(R.id.radioCelsiusTo);
+        toKelvin = findViewById(R.id.radioKelvinTo);
+
+        tempResult = 0;
+        calc = new TemperatureTransform();
+
+        convertBtn.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleClick();
+            }
+        });
+
+        fromGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                toFarenheit.setEnabled(true);
+                toCelsius.setEnabled(true);
+                toKelvin.setEnabled(true);
+
+                if (checkedId == fromFarenheit.getId()) {
+                    toFarenheit.setEnabled(false);
+                }
+                if (checkedId == fromCelsius.getId()) {
+                    toCelsius.setEnabled(false);
+                }
+                if (checkedId == fromKelvin.getId()) {
+                    toKelvin.setEnabled(false);
+                }
+            }
+        });
+
+        toGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                fromFarenheit.setEnabled(true);
+                fromCelsius.setEnabled(true);
+                fromKelvin.setEnabled(true);
+
+                if (checkedId == toFarenheit.getId()) {
+                    fromFarenheit.setEnabled(false);
+                }
+                if (checkedId == toCelsius.getId()) {
+                    fromCelsius.setEnabled(false);
+                }
+                if (checkedId == toKelvin.getId()) {
+                    fromKelvin.setEnabled(false);
+                }
+            }
+        });
+    }
+
+    private void handleClick() {
+        if (inputText.getText().equals("")) {
+            Toast.makeText(CalculatorActivity.this, "You need to input a value to convert.",
+                    Toast.LENGTH_SHORT).show();
+        }
+
+        try {
+            double tempValue = Double.parseDouble(String.valueOf(inputText.getText()));
+            convertAndSend(tempValue);
+        } catch (Exception e) {
+            Toast.makeText(CalculatorActivity.this, "You need to input a numeric value to convert.",
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void convertAndSend(double tempValue) {
+        int selectedId = fromGroup.getCheckedRadioButtonId();
+
     }
 }
