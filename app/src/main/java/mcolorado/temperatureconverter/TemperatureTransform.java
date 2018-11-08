a100d3ae182dd83ab5f7c966dadf516dde7fe975
Manuel Colorado <manuel1412@gmail.com>
2018-11-08 19:49:58 +0000

    Added formulae

---
+++ app/src/main/java/mcolorado/temperatureconverter/TemperatureTransform.java
@@ -0,0 +1,30 @@
+package mcolorado.temperatureconverter;
+
+public class TemperatureTransform {
+
+    public double farenheitToCelsius(double degreesInF) {
+        return (degreesInF - 32) * 5 / 9;
+    }
+
+    public double celsiusToFarehnheit(double degreesInC) {
+        return degreesInC * 9 / 5 + 32;
+    }
+
+    public double kelvinToFarenheit(double degreesInK) {
+        return degreesInK * 9 / 5 - 459.67;
+    }
+
+    public double farenheitToKelvin(double degreesInF) {
+        return (degreesInF + 459.67) * 5 / 9;
+    }
+
+    public double kelvinToCelsius(double degreesInK) {
+        return degreesInK - 273.15;
+    }
+
+    public double celsiusToKelvin(double degreesInC) {
+        return degreesInC + 273.15;
+    }
+
+
+}
